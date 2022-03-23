7263e11ef1f78285297bea3d3e68850458b00221
Thijs van Hoof <thijs.v.h@gmail.com>
2022-03-23 18:19:18 +0100

    Update getGitHubEventDates.go

--- getGitHubEventDates.go
+++ getGitHubEventDates.go
@@ -18,49 +18,51 @@ func getGitHubEventDates(account string) ([]string, error) {
 	dates := []string{}
 	url := fmt.Sprintf("https://api.github.com/users/%v/events?per_page=100", account)
 	for url != "" {
-		next, err := addDatesFromGitHubEventPage(url, dates)
+		pageDates, next, err := getGitHubEventPageDates(url)
 		if err != nil {
 			return nil, err
 		}
+		dates = append(dates, pageDates...)
 		url = next
 	}
 	return dates, nil
 }
 
 /*
- * Adds all the "created_at" dates found in the GitHub event page to the provided
- * "dates" slice and returns the url for the next GitHub event page, if it exists
+ * Get all "created_at" dates from the provided GitHub event page URL.
+ * Also returns the URL for the next GitHub event page, if it exists.
  */
-func addDatesFromGitHubEventPage(url string, dates []string) (string, error) {
+func getGitHubEventPageDates(url string) ([]string, string, error) {
 	// Create HTTP client and request
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return "", err
+		return nil, "", err
 	}
 
 	// Add headers and perform request
 	req.Header.Add("accept", "application/vnd.github.v3+json")
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", err
+		return nil, "", err
 	}
 
 	// Read the response body
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return nil, "", err
 	}
 
 	// Get events from response body
 	var githubEvents []GithubEvent
 	err = json.Unmarshal(body, &githubEvents)
 	if err != nil {
-		return "", err
+		return nil, "", err
 	}
 
-	// Append "created_at" dates
+	// Extract "created_at" dates
+	dates := []string{}
 	for _, event := range githubEvents {
 		dates = append(dates, event.CreatedAt)
 	}
@@ -68,5 +70,5 @@ func addDatesFromGitHubEventPage(url string, dates []string) (string, error) {
 	// Retrieve the next page URL
 	link := resp.Header.Get("link")
 	next := getLinkHeaderURI(link, "next")
-	return next, nil
+	return dates, next, nil
 }
